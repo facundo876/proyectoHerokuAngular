8dd66e81d0247a1eea27591764717d5e5989a72d
Facundo Arce <facundo.ea07@gmail.com>
2021-08-27 10:59:59 -0300

    Sexto correciones

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const path = require('path');
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/MiPrimerProyecto'));
 
-app.use(express.static(__dirname + '/MiPrimerProyecto'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/MiPrimerProyecto/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + '/MiPrimerProyecto/index.html'));
-})
-
-console.log('Build successfull!!');
