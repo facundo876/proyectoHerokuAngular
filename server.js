56cf5d21862f458cc930c656fdbdd22c973d752c
Facundo Arce <facundo.ea07@gmail.com>
2021-08-27 11:22:09 -0300

    Septimo2 correciones

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
-
 const app = express();
-
-const path = require('path');
+const paht = require('path');
 
 app.use(express.static(__dirname + '/angularapp'));
 
 app.listen(process.env.PORT || 8080);
 
-app.get('*', function(req, res){
+app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/angularapp/index.html'));
-})
+});
 
-console.log('Build successfull!!');
+console.log('Finalizo!!');
